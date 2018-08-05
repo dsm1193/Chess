f7457a265170c02bec7d598bffa1f160d799b538
Dennis Moon <dsm1193@gmail.com>
2018-08-05 17:42:51 -0400

    Added coloring to board

--- display.rb
+++ display.rb
@@ -25,31 +25,47 @@ class Display
 
   def render
     system('clear')
+    puts "Arrow keys, WASD, or vim to move, space or enter to confirm."
     current_cursor_pos = @cursor.cursor_pos
-
+# debugger
     @board.rows.each.with_index do |row,idx|
       row.each.with_index do |square,idx2|
-        if current_cursor_pos == [idx,idx2]
-          if square.class == NullPiece && @cursor.selected == true
-            print "[ ]".center(3).colorize(:red)
-          elsif square.class == NullPiece
-            print "[ ]".center(3).colorize(:blue)
-          else
-            if @cursor.selected == true
-              print  "[#{square.symbol.encode('utf-8')}]".center(3).colorize(:red)
-            else
-              print "[#{square.symbol.encode('utf-8')}]".center(3).colorize(:blue)
-            end
-          end
-        elsif square.class == NullPiece
-          print " ".center(3)
+        if cursor.cursor_pos == [idx,idx2] && @cursor.selected
+          print "#{square.symbol}".center(3).colorize( background: :magenta )
+        elsif cursor.cursor_pos == [idx,idx2]
+          print "#{square.symbol}".center(3).colorize( background: :light_magenta )
+        elsif (idx + idx2).odd?
+          print "#{square.symbol}".center(3).colorize( background: :light_white )
         else
-          print  "#{square.symbol.encode('utf-8')}".center(3)
+          print "#{square.symbol}".center(3).colorize( background: :light_black )
         end
       end
       print "\n"
     end
     @notifications.each { |_, val| puts val }
+
+    #
+    #     if current_cursor_pos == [idx,idx2]
+    #       if square.class == NullPiece && @cursor.selected == true
+    #         print "[ ]".center(3).colorize(:red)
+    #       elsif square.class == NullPiece
+    #         print "[ ]".center(3).colorize(:blue)
+    #       else
+    #         if @cursor.selected == true
+    #           print  "[#{square.symbol.encode('utf-8')}]".center(3).colorize(:red)
+    #         else
+    #           print "[#{square.symbol.encode('utf-8')}]".center(3).colorize(:blue)
+    #         end
+    #       end
+    #     elsif square.class == NullPiece
+    #       print " ".center(3)
+    #     else
+    #       print  "#{square.symbol.encode('utf-8')}".center(3)
+    #     end
+    #   end
+    #   print "\n"
+    # end
+    # @notifications.each { |_, val| puts val }
   end
 
   def change_cursor
